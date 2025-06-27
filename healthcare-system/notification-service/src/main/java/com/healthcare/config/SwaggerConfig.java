f9e80bae62d3ebf48db191162c69abf6cb7d6ed5
truthseeker-ai <132370747+truthseeker-ai@users.noreply.github.com>
2025-06-27 12:00:39 +0530

    Add healthcare-system/notification-service/src/main/java/com/healthcare/config/SwaggerConfig.java 1751005839690

---
+++ healthcare-system/notification-service/src/main/java/com/healthcare/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.healthcare.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI notificationServiceAPI() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("Notification Service API")
+                .description("REST API documentation for the Notification Service")
+                .version("1.0.0")
+                .license(new License()
+                    .name("Apache 2.0")
+                    .url("http://springdoc.org")))
+            .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+            .components(new io.swagger.v3.oas.models.Components()
+                .addSecuritySchemes("Bearer Authentication", new SecurityScheme()
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("bearer")
+                    .bearerFormat("JWT")
+                    .description("Enter JWT token")));
+    }
+}
